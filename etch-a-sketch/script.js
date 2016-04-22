5254abe6e53bf3566f8fcec704cf06c5828f6419
Bradley Zellman <bzellman@got2jam.com>
2016-04-22 11:17:07 -0500

    Chanced fuction from mutating table to simpley drawing a new one

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,62 +1,13 @@
-// alert('Welcome to JavaScript');
-// console.log("TEST");
 
 console.log("Value");	
 $(document).ready(function(){
-	// console.log("TEST");
-	$('div').css("background-color","yellow");
-
 
 	$('#button').click(function(){
 		var gridSize = document.getElementById('gridSizeValue').value;
-		var amountOfTableRows = document.getElementsByClassName('tableRow').length;
-		setTableRows(amountOfTableRows, gridSize);
+		drawNewTable(gridSize);
 	});
-	
 	styleTable();
 });
-
-function setTableRows(currentAmountOfRows, newGridSize) {
-	// check if even number 
-	// Check if the number of rows is less than or greater than current amount of rows
-	// either add or subtract rows
-	// loop through rows and either add or subtract columns 
-
-		if (newGridSize > currentAmountOfRows) {
-			var rowsToAdd = newGridSize - currentAmountOfRows;
-			for (var i = 0; i < rowsToAdd; i++) {
-				$('.mainTable').append("<tr class=\"tableRow\"></tr>");
-			}
-			newAmountOfRows = document.getElementsByClassName('tableRow');
-			for (var i = 0; i < newAmountOfRows.length; i++) {
-				currentAmountOfColumnsInRow = newAmountOfRows[i].getElementsByClassName('tableColumn').length;
-				columnsToAdd = newGridSize - currentAmountOfColumnsInRow;
-				// console.log("Need to add " + columnsToAdd + "columns");
-				for (var j = 0; j < columnsToAdd; j++) {
-					$('.tableRow:nth-child(' + (i+1) +')').append("<td class=\"tableColumn\">");	
-				}
-			}
-		} 
-
-
-		else if (newGridSize < currentAmountOfRows){
-			var rowsToSubtract = currentAmountOfRows - newGridSize;
-			for (var i = 0; i < rowsToSubtract; i++) {
-				$('.tableRow:nth-child(' + (i+1) +')').remove();
-			}
-
-			newAmountOfRows = document.getElementsByClassName('tableRow');
-			for (var i = 0; i < newAmountOfRows.length; i++) {
-				currentAmountOfColumnsInRow = newAmountOfRows[i].getElementsByClassName('tableColumn').length;
-				columnsToSubtract = currentAmountOfColumnsInRow - newGridSize;
-				// console.log("There are " + currentAmountOfColumnsInRow + " columns in row" + (i+1));
-				for (var j = 0; j < columnsToSubtract; j++) {
-					$('.tableColumn:nth-child(' + (i+1) +')').remove();
-				}
-			}
-		} 
-		styleTable();
-} 
 
 function styleTable(){
 
@@ -79,4 +30,23 @@ function styleTable(){
 	cellHeightInt= Number(dynamicCellHeight);
 	$('td').height(cellHeightInt);
 	$('td').width(cellHeightInt);
+}
+
+
+function drawNewTable(newGridSize){
+	$('.mainTable').remove();
+	// Draw New Grid -> Add Table -> Add Rows -> Add Column
+	$('.tableDiv').append("<table class='mainTable'>")
+	
+		for(var i = 0; i < newGridSize; i++){
+			$('.mainTable').append("<tr class='tableRow'>");
+			
+				for(var j = 0; j < newGridSize; j++){
+				$('.tableRow:nth-child(' + (i+1) +')').append("<td class=\"tableColumn\">");
+				}
+			$('.mainTable').append("</tr>");
+		}
+	$('.tableDiv').append("</table>");
+	
+	styleTable();
 }
